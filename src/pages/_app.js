bf14d7c201386a6749ecdc19f457137065076f06
RicardoSantosSantana <ricardo@needsolution.com.br>
2022-07-20 07:22:03 -0300

    second commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ export default function MyApp({ Component, pageProps }) {
    <>
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />  
-      <link  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"   crossOrigin="anonymous"/>
+      <link async href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"   crossOrigin="anonymous"/>
       </Head>
       
       <div className="container">
@@ -24,7 +24,7 @@ export default function MyApp({ Component, pageProps }) {
           </div>    
       </div>
     
-      <script  src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossOrigin="anonymous"/>
+      <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossOrigin="anonymous"/>
       </>
   )
 }
